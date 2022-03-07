166e7e987d0116902cf4f2d21e21e2ff7f17fd71
Raven <5958823+Raven-bit@users.noreply.github.com>
2022-03-07 03:00:54 +0000

    Update install.sh
    Add comment documenting why we use vim-nox.

--- install.sh
+++ install.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 if grep -P --quiet '(?<=ID=)debian$' /etc/os-release; then
+  # So, debian/raspberry pi os's default vim target has X11 support but is _missing_
+  # python3 support. Which a bunch of my mods require. OTOH, vim-nox has this.
+  # Why? I don't know, this seems silly.
   sudo apt-get remove --purge vim vim-runtime vim-gnome vim-tiny vim-common vim-gui-common -y
   sudo apt-get install vim-nox -y
 fi
