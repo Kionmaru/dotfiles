eb12f3b30c2ccdb44d0cf796ddae1c8ac24ee569
Raven-bit <5958823+Raven-bit@users.noreply.github.com>
2024-05-01 15:29:38 -0700

    Integrate C-hjkl with neovim panes

--- .config/nvim-kickstart/lua/kickstart/plugins/lspconfig.lua
+++ .config/nvim-kickstart/lua/kickstart/plugins/lspconfig.lua
@@ -139,7 +139,11 @@ return {
         -- clangd = {},
         -- gopls = {},
         -- pyright = {},
-        -- rust_analyzer = {},
+        rust_analyzer = {
+          procMacro = {
+            enable = true
+          },
+        },
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
